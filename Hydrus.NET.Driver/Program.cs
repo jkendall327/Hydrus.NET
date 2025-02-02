edf0e33eb98137873ce5d272854d2134f102f362
jackkendall <jkendall3096@gmail.com>
2025-02-02 13:50:18 +0000

    Add driver app for simple testing

---
+++ Hydrus.NET.Driver/Program.cs
@@ -0,0 +1,8 @@
+﻿using Hydrus.NET;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+var app = builder.Build();
+
+var hydrusClient = new HydrusClient(null, null);
