a4a12623cb96b969ac2f49befed9d86b7f5ca1a7
jackkendall <jkendall3096@gmail.com>
2025-02-02 13:53:19 +0000

    Basic test in place

--- Hydrus.NET.Driver/Program.cs
+++ Hydrus.NET.Driver/Program.cs
@@ -1,8 +1,14 @@
 ﻿using Hydrus.NET;
-using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder();
 
 var app = builder.Build();
 
-var hydrusClient = new HydrusClient(null, null);
+var baseUrl = builder.Configuration["Hydrus:BaseUrl"];
+var accessKey = builder.Configuration["Hydrus:AccessKey"];
+
+var hydrusClient = new HydrusClient(baseUrl, accessKey);
+
+var version = await hydrusClient.Client.GetVersionAsync();
+
+Console.WriteLine(version);
