43436fd0ee13f37e3f237af332e6632dc2a9e648
jackkendall <jkendall3096@gmail.com>
2025-02-02 09:10:34 +0000

    Add first method

--- Hydrus.NET/Features/HydrusClientManager.cs
+++ Hydrus.NET/Features/HydrusClientManager.cs
@@ -1,6 +1,24 @@
+using System.Text.Json.Serialization;
+
 namespace Hydrus.NET;
+
+public record HydrusVersion(
+    int Version, 
+    int ClientVersion)
+{
+    [JsonPropertyName("version")]
+    public int Version { get; init; } = Version;
+    
+    [JsonPropertyName("client_version")]
+    public int ClientVersion { get; init; } = ClientVersion;
+}
 
 public class HydrusClientManager(HttpClient httpClient)
 {
     private HttpClient _httpClient = httpClient;
+
+    public async Task<HydrusVersion> GetHydrusVersionAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
