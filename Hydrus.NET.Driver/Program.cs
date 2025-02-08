cb9934f1f17f73b7d7d4393651ce0fdbd057e223
jackkendall <jkendall3096@gmail.com>
2025-02-08 09:30:55 +0000

    Add driver code for exercising times

--- Hydrus.NET.Driver/Program.cs
+++ Hydrus.NET.Driver/Program.cs
@@ -12,9 +12,52 @@ var app = builder.Build();
 
 var client = app.Services.GetRequiredService<HydrusClient>();
 
-var version = await client.Client.GetVersionAsync();
+await TestClientManagement(client);
 
-await client.Cookies.SetCookiesAsync([new("PHPSESSID", "07669eb2a1a6e840e498bb6e0799f3fb", "somesite.com", "/", 1627327719)]);
-var u = await client.Cookies.GetCookiesAsync("somesite.com");
+await TestCookies(client);
 
-Console.WriteLine(version);
+await TestTimes(client.Times);
+
+return;
+
+async Task TestTimes(HydrusTimeManager timeManager)
+{
+    var file = new HydrusFiles { FileId = 123456 };
+
+    Console.WriteLine("Incrementing file view time...");
+    await timeManager.IncrementFileViewtimeAsync(
+        files: file,
+        canvasType: HydrusTimeManager.CanvasTypes.MediaViewer,
+        views: 2,
+        viewtime: 15.5f
+    );
+
+    Console.WriteLine("Setting file view time...");
+    await timeManager.SetFileViewtimeAsync(
+        files: file,
+        canvasType: HydrusTimeManager.CanvasTypes.PreviewViewer,
+        views: 10,
+        viewtime: 120.0f
+    );
+
+    Console.WriteLine("Setting a timestamp...");
+    await timeManager.SetTimeAsync(
+        files: file,
+        timestampType: HydrusTimeManager.TimestampTypes.FileImportTime,
+        timestamp: DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+    );
+}
+
+async Task TestClientManagement(HydrusClient hydrusClient)
+{
+    var hydrusVersion = await hydrusClient.Client.GetVersionAsync();
+    
+    Console.WriteLine(hydrusVersion);
+}
+
+async Task TestCookies(HydrusClient hydrusClient)
+{
+    var cookie = new HydrusCookie("PHPSESSID", "07669eb2a1a6e840e498bb6e0799f3fb", "somesite.com", "/", 1627327719);
+    await hydrusClient.Cookies.SetCookiesAsync([cookie]);
+    var u = await hydrusClient.Cookies.GetCookiesAsync("somesite.com");
+}
