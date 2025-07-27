eb6f4c702dd8eca59019aaf6d7dcc6ad375f2f2a
jackkendall <jkendall3096@gmail.com>
2025-07-27 13:08:06 +0100

    Begin documentation

--- Hydrus.NET/Models/HydrusOptions.cs
+++ Hydrus.NET/Models/HydrusOptions.cs
@@ -1,7 +1,17 @@
 namespace Hydrus.NET;
 
+/// <summary>
+/// Options for configuring access to a Hydrus API instance.
+/// </summary>
 public class HydrusOptions
 {
-    public string BaseUrl { get; set; }
-    public string AccessKey { get; set; }
+    /// <summary>
+    /// The base URL the API is served from.
+    /// </summary>
+    public required string BaseUrl { get; set; }
+    
+    /// <summary>
+    /// The access key you will use to authenticate with the client.
+    /// </summary>
+    public required string AccessKey { get; set; }
 }
