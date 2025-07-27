6342f7f9b953d7bf1e79cebcbd92e41f2bfaa65e
jackkendall <jkendall3096@gmail.com>
2025-07-27 13:23:14 +0100

    Clean up error types

--- Hydrus.NET/Models/HydrusException.cs
+++ Hydrus.NET/Models/HydrusException.cs
@@ -2,23 +2,34 @@ namespace Hydrus.NET;
 
 using System;
 
+/// <summary>
+/// Represents an error returned by the Hydrus API.
+/// </summary>
 public sealed class HydrusException : Exception
 {
-    public string HydrusExceptionType { get; }
+    /// <summary>
+    /// Represents the kind of the exception.
+    /// </summary>
+    public string Type { get; }
+    
+    /// <summary>
+    /// Contains the error's message.
+    /// </summary>
     public string HydrusError { get; }
 
-    public HydrusException(string hydrusExceptionType, string hydrusError) : base(hydrusError)
+    /// <summary>
+    /// Creates a new instance of the exception.
+    /// </summary>
+    /// <param name="type">The type of the error.</param>
+    /// <param name="hydrusError">The error message.</param>
+    public HydrusException(string type, string hydrusError) : base(hydrusError)
     {
-        HydrusExceptionType = hydrusExceptionType;
+        Type = type;
         HydrusError = hydrusError;
     }
 
-    public HydrusException(string hydrusExceptionType, string hydrusError, Exception innerException)
-        : base(hydrusError, innerException)
-    {
-        HydrusExceptionType = hydrusExceptionType;
-        HydrusError = hydrusError;
-    }
-
-    public override string ToString() => $"{HydrusExceptionType}: {HydrusError}";
+    /// <summary>
+    /// <inheritdoc cref="Object.ToString()"/>
+    /// </summary>
+    public override string ToString() => $"{Type}: {HydrusError}";
 }
